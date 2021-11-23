8da3c8a2033697fd16916303fd25134da38bfe1f
Austin Flores <ctrl4ltdeleteme@gmail.com>
2021-11-23 17:44:20 +0100

    Test interactions with test net

---
+++ README.md
@@ -0,0 +1,7 @@
+PREREQS:
+* Install Metamask browser plugin
+* Set up test nets in metamask
+* Have python 3.x interpreter installed
+
+
+Replace __NODE__ with actual test node
---
+++ transfer_ether.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+
+from eth_account import Account
+from web3 import Web3, HTTPProvider
+from web3.gas_strategies.time_based import medium_gas_price_strategy
+
+
+#Test network, node that manu has on ropsten test ethereum network
+site="https://ropsten.infura.io/v3/__NODE__"
+#my metamask address on the ropsten network
+metamask_address='0x73D8b0742D563869D8197eC13cF3Fe6Ef5552Adc'
+private_key_secret='Something' # any secret from which your private key will be generated
+
+w3 = Web3(HTTPProvider(site,request_kwargs={'timeout':60}))
+
+# block = w3.eth.get_block(12345)
+# print(block)
+# exit()
+
+private_key = w3.sha3(text = private_key_secret)
+print('private key:', private_key)
+
+acct = w3.eth.account.privateKeyToAccount(private_key)
+# print(acct)
+print('address:', acct.address)
+print ('balance:', Web3.fromWei(w3.eth.get_balance(acct.address), 'ether'))
+
+tx = dict(
+    nonce=w3.eth.get_transaction_count(acct.address),
+    maxFeePerGas=3000000000,
+    maxPriorityFeePerGas=2000000000,
+    gas=100000,
+    to=metamask_address,
+    value=Web3.toWei(0.01, 'ether'),
+    data=b'',
+    type=2,
+    chainId=3,
+  )
+
+signed_txn = w3.eth.account.sign_transaction(tx , private_key)
+tx_id = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
+print('signed_txn', signed_txn)
+print('tx_id: ', tx_id)
+
+exit()
